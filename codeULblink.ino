6b3f11ed37eef723b900d55e2bceaa87195b25ff
eirfanshh <eirfans100@gmail.com>
2025-06-23 22:32:51 +0800

    Added dual ultrasonic sensor support and bin full Serial message feature

--- codeULblink.ino
+++ codeULblink.ino
@@ -1,70 +1,99 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo;
 byte pos = 0;
 
-// pin connections for HC-SR04 sensor 
-const int trigPin = 5;
-const int echoPin = 4;
-long distance;
+// Sensor 1 (for hand detection)
+const int trigPin1 = 5;
+const int echoPin1 = 4;
+
+// Sensor 2 (for bin fullness detection)
+const int trigPin2 = 3;
+const int echoPin2 = 2;
+
+// LEDs
+const int standbyLED = 13;
+const int binFullLED = 12;
+
+long distance1;
+long distance2;
 
 void setup() {
-  // set pin modes for HC-SR04
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(13, OUTPUT); // red LED on controller board
+  // Initialize Serial Monitor
+  Serial.begin(9600);
 
-  myservo.attach(6);  // connect servo to pin 6
+  // Servo and LED pins
+  myservo.attach(6);
+  pinMode(standbyLED, OUTPUT);
+  pinMode(binFullLED, OUTPUT);
 
-  // open the lid
-  for (pos = 8; pos < 100; pos += 1) {
-    myservo.write(pos);
-    delay(15);
-  }
+  // Sensor 1 pins
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
 
-  // close the lid
-  for (pos = 100; pos > 8; pos -= 1) {
-    myservo.write(pos);
-    delay(15);
-  }
+  // Sensor 2 pins
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+
+  // Initialize servo position (closed)
+  myservo.write(8);
 }
 
 void loop() {
-  // red LED blinks slowly
-  digitalWrite(13, HIGH);
+  // Blink standby LED slowly
+  digitalWrite(standbyLED, HIGH);
   delay(500);
-  digitalWrite(13, LOW);
+  digitalWrite(standbyLED, LOW);
   delay(500);
 
-  // trigger ultrasonic sensor
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(20);
-  digitalWrite(trigPin, LOW);
-  distance = pulseIn(echoPin, HIGH);
+  // Get distance from Sensor 1 (for hand detection)
+  distance1 = getDistance(trigPin1, echoPin1);
 
-  // if an object is detected within 30cm
-  if (distance < 3000) {
-    digitalWrite(4, HIGH); // turn on white LED
+  // If object detected within 30cm, open lid
+  if (distance1 < 300) {
+    openLid();
+    delay(8000);  // Keep lid open for 8 seconds
+    closeLid();
+  }
 
-    // open the lid
-    for (pos = 8; pos < 100; pos += 1) {
-      myservo.write(pos);
-      delay(15);
-    }
+  // Get distance from Sensor 2 (to check if bin is full)
+  distance2 = getDistance(trigPin2, echoPin2);
 
-    // red LED blinks fast
-    for (pos = 0; pos < 25; pos++) {
-      digitalWrite(13, HIGH);
-      delay(100);
-      digitalWrite(13, LOW);
-      delay(100);
-    }
-
-    // close the lid
-    for (pos = 100; pos > 8; pos -= 1) {
-      myservo.write(pos);
-      delay(15);
-    }
+  // If bin full detected (less than 10cm remaining space)
+  if (distance2 < 100) {
+    digitalWrite(binFullLED, HIGH);
+    Serial.println("FULL");  // Display 'FULL' on Serial Monitor
+  } else {
+    digitalWrite(binFullLED, LOW);
   }
 }
+
+// Function to measure distance from an HC-SR04 sensor
+long getDistance(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = (duration * 0.034) / 2; // in cm
+  return distance;
+}
+
+// Function to open the lid
+void openLid() {
+  for (pos = 8; pos < 100; pos++) {
+    myservo.write(pos);
+    delay(15);
+  }
+}
+
+// Function to close the lid
+void closeLid() {
+  for (pos = 100; pos > 8; pos--) {
+    myservo.write(pos);
+    delay(15);
+  }
+}
+
 
